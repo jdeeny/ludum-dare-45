83c423c76799298051f0b1d8902e895ccffab00e
jdeeny <jdeeny@gmail.com>
2019-10-07 14:50:51 -0600

    Update build win

--- build_win.bat
+++ build_win.bat
@@ -1,10 +1,10 @@
 :: Needs to be updated
 
-::zip -r Brugga.zip assets entities gamestates lib physics player ui conf.lua main.lua README.md
-::rename Brugga.zip Brugga.love
-::mkdir release\Brugga_Windows
-::copy /b love.exe+Brugga.love release\Brugga_Windows\Brugga.exe
-::del Brugga.love
-::copy release\dist\* release\Brugga_Windows
-::cd release
-::zip -r Brugga_Windows.zip Brugga_Windows
+zip -r BirthdaySuit.zip assets entities gamestates lib physics player ui conf.lua main.lua README.md
+rename BirthdaySuit.zip BirthdaySuit.love
+mkdir release\BirthdaySuit_Windows
+copy /b love.exe+BirthdaySuit.love release\BirthdaySuit_Windows\BirthdaySuit.exe
+del Brugga.love
+copy release\dist\* release\BirthdaySuit_Windows
+cd release
+zip -r BirthdaySuit_Windows.zip BirthdaySuit_Windows
